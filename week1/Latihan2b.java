7e836e23f6fdf215cfd95fbe011827ca43ab43d6
tegar2003 <101033408+tegar2003@users.noreply.github.com>
2022-03-06 15:40:56 +0700

    Update Latihan2b.java

--- week1/Latihan2b.java
+++ week1/Latihan2b.java
@@ -1,9 +1,9 @@
 package week1;
-import java.util.Scanner;
+
 
 public class Latihan2b {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
+        
         int a=6, t=8;
         double luas;
         
